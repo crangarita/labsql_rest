8a5b3eb878802499bf3ab98fe3b9067e5f10cad0
Carlos Rene Angarita Sanguino <crangarita@gmail.com>
2023-06-25 20:27:27 -0500

    Inicio del proyecto del laboratorio

---
+++ src/main/java/link/softbond/LabsqlrestApplication.java
@@ -0,0 +1,13 @@
+package link.softbond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsqlrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabsqlrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/link/softbond/LabsqlrestApplicationTests.java
@@ -0,0 +1,13 @@
+package link.softbond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsqlrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
