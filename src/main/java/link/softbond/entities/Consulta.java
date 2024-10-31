aeca45d93dc1f6ba3d571f6713eefb2dfb8f972a
cquintero99 <ciroalfonsoqude@ufps.edu.co>
2024-10-31 06:51:49 -0500

    add ruta bd railway

--- src/main/java/link/softbond/entities/Consulta.java
+++ src/main/java/link/softbond/entities/Consulta.java
@@ -15,42 +15,41 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
-@Entity 
+@Entity
 @Data
-public class                                  Consulta implements Serializable {
+public class Consulta implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	private String descripcion;
-	
+
 	private String explicacion;
-	
+
 	@JsonIgnore
 	private String solucion;
-	
+
 	@JsonIgnore
 	private String explicsolucion;
-	
+
 	@JsonIgnore
 	private String solucionalternativa;
-	
+
 	private Integer numpracticas;
-	
+
 	private Integer estado;
 
-	
 	@ManyToOne
-	@JoinColumn(name="idproblema")
+	@JoinColumn(name = "idproblema")
 	private Problema problema;
-	
-	@OneToMany(mappedBy="consulta")
+
+	@OneToMany(mappedBy = "consulta")
 	@JsonIgnore
 	private List<Practica> practicas;
-	
-	@OneToMany(mappedBy="consulta")
+
+	@OneToMany(mappedBy = "consulta")
 	@JsonIgnore
 	private List<Opcion> opciones;
 
